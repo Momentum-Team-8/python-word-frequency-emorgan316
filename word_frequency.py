8401ae030b8217a7ce75f62d7f80944820b7e9f4
emorgan316 <elliemorgan96@gmail.com>
2021-06-10 08:31:12 -0400

    still trying to figure out how to implement STOP WORDS

--- word_frequency.py
+++ word_frequency.py
@@ -4,35 +4,40 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
-file = open('praise_song_for_the_day.txt')
-file
+# file = open('praise_song_for_the_day.txt')
+# file
 
-for i in range(8):
-    print(repr(praise.readline()))
+# for i in range(8):
+#     print(repr(praise.readline()))
 
-def clean_text(file):
-    """given text returns text all lowercase, no punctuation, and removes all stop words"""
-    text = praise.lower
+# def clean_text(file):
+#     """given text returns text all lowercase, no punctuation, and removes all stop words"""
+#     text = praise.lower
+
+# def print_word_freq(file):
+#     """Read in `file` and print out the frequency of words in that file."""
+#     readfile()
+#     pass
 
 
 def print_word_freq(file):
+    with open(file) as words:
+        text = words.read().lower()
+        print(text)
     """Read in `file` and print out the frequency of words in that file."""
-    readfile()
-
-    pass
-
-
 
 
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
 
+    
     parser = argparse.ArgumentParser(
         description='Get the word frequency in a text file.')
     parser.add_argument('file', help='file to read')
     args = parser.parse_args()
 
+    
     file = Path(args.file)
     if file.is_file():
         print_word_freq(file)
